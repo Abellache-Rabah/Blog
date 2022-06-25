9a583aab222d2e08ded4e4f27f910d9b34af38f2
ABELLACHE Rabah <agar3arab@gmail.com>
2022-06-25 12:24:27 +0100

    function blog using  mongoose

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const ejs = require("ejs");
 const _ = require('lodash');
 const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const aboutContent = "hello.";
 
 const app = express();
 
@@ -16,58 +14,78 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost:27017/blog", {useNewUrlParser : true});
+mongoose.connect("mongodb://localhost:27017/blog");
 
 
 /*globel*/
 const posts=[];
-const postSchema = {
-  title : String,
-  body : String
-};
+const postSchema = new mongoose.Schema({ title: 'string',  body : "string"});
+
 const Post = mongoose.model("post" , postSchema);
+
 const homePost = new Post ({
-  title : "Home",
-  body : "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
+  title : "Home Page",
+  body : "welcome to rabah's blog a personal blog by rabah abellache if u want to know about me go to the about me page in navbar or you wana contact me go to contact page above!."
 });
-const aboutPost = new Post({
-  title:"About",
-  body : "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui."
-});
-const contactPost = new Post ({
-  title : "Contact",
-  body : "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero."
-});
+
+const defaultPosts =[homePost];
+
+
+
+
 
 
 
 app.get("/",function(req , res){
 
-  res.render("home", {
-    homeStartingContent : homeStartingContent,
-    posts: posts
-  });
+  Post.find({},function(err,result){
+    if(err){
+      res.redirect("/")
+    }else {
+    res.render("home", {homeStartingContent: homePost.body, posts: result});
+      
+    }
 });
+
+});
+
+
+
+
+
+
+
+
+
+
+
 app.get("/posts/:topic" ,function(req , res){
   const reqTitle = _.lowerCase(req.params.topic);
  
-
-  posts.forEach(function(post){
-    const postsTitle = _.lowerCase(post.title);
-    
-    if (reqTitle === postsTitle){
-      res.render("post",{
-        postsTitle: post.title,
-        postBody : post.body
-      });
-      
-    }else {
+  Post.findById(req.params.topic, function (err, results) {
+    if (err) {
+      console.log(err);
       res.render("not-found");
+    } else {
+      res.render("post",{
+        postsTitle: results.title,
+        postBody : results.body
+      });
     }
   });
-
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.get("/about",function(req , res){
@@ -77,30 +95,26 @@ app.get("/about",function(req , res){
   
 });
 
-app.get("/contact",function(req , res){
-  res.render("contact", {
-    contactContent : contactContent
-  });
+
+
+app.get("/zidpost",function(req , res){
+  res.render("zidpost");
   
 });
 
-app.get("/compose",function(req , res){
-  res.render("compose");
-  
-});
+app.post("/zidpost",function(req , res){
 
-app.post("/compose",function(req , res){
-  
-
-  const post ={
+  const post = new Post ({
     title : req.body.postTitle,
     body : req.body.postBody
-  };
-  posts.push(post);
+  });
+
+  post.save();
   res.redirect("/");
 
-
 });
+
+
 
 
 
